2bd635a192dca9b10a2ea24759d0c43e22455db0
Ludovic Tankeu <ludovic.tankeu@yahoo.com>
2022-04-27 12:09:53 +0100

    chore: tetris game works!

---
+++ cross-platform-monorepo/projects/game-engine-lib/src/lib/game-engine-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-game-engine-lib',
+  template: `
+    <p>
+      game-engine-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GameEngineLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ cross-platform-monorepo/projects/game-engine-lib/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of game-engine-lib
+ */
+
+export * from './lib/services/game-engine-lib.service';
+export * from './lib/game-engine-lib.component';
+export * from './lib/game-engine-lib.module';
+export * from "./lib/components/board/board.component";
